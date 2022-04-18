095705323a906bcb3b8dfdb308b8e6b589cce0a4
Yussini <Yusif.abdalov@yandex.ru>
2022-04-18 11:45:50 +0300

    feat(реализовал лайк и удаление карточки)

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,40 @@ let bioName = document.querySelector('.bio__name');
 let bioDescription = document.querySelector('.bio__description');
 let nameInput = document.querySelector('.popup__input_type_call');
 let jobInput = document.querySelector('.popup__input_type_hobbies');
+let listContainer = document.querySelector('.elements__grid');
+let template = document.querySelector('.template')
+let deleteBtn = document.querySelector('.elements__dlt-btn');
+
+function render() {
+  const html = initialCards.map(getElement);
+  listContainer.append(...html);
+}
+
+function getElement(item) {
+  const getElementTemplate = template.content.cloneNode(true);
+  const templateName = getElementTemplate.querySelector('.elements__name');
+  const templateImage = getElementTemplate.querySelector('.elements__photo');
+  const deleteBtn = getElementTemplate.querySelector('.elements__dlt-btn');
+  const likeBtn = getElementTemplate.querySelector('.elements__like-btn')
+
+  templateImage.src = item.link;
+  templateName.textContent = item.name;
+
+  deleteBtn.addEventListener('click', removeElement);
+  likeBtn.addEventListener('click', likeElement);
+
+  return getElementTemplate;
+}
+
+function likeElement(){
+  this.classList.toggle('button_active');
+}
+
+
+function removeElement(evt){
+  const dltElement = evt.target.closest('.elements__item');
+  dltElement.remove();
+}
 
 function openModalWindow() {
   modalWindow.classList.add('popup_is-active');
@@ -27,3 +61,32 @@ function formSubmitHandler(evt) {
 editProfile.addEventListener('click', openModalWindow);
 modalCloseBtn.addEventListener('click', closeModalWindow);
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+render();
