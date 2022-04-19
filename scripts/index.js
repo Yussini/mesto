a9bd02077f66750d37c3c11efde0fb24b708b099
Yussini <Yusif.abdalov@yandex.ru>
2022-04-19 14:14:18 +0300

    feat(сделал возможность добавления своей карточки)

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,3 @@
-let editProfile = document.querySelector('.bio__edit-btn');
-let modalWindow = document.querySelector('.popup');
-let modalCloseBtn = document.querySelector('.popup__close-btn');
-let formElement = document.querySelector('.popup__form');
-let bioName = document.querySelector('.bio__name');
-let bioDescription = document.querySelector('.bio__description');
-let nameInput = document.querySelector('.popup__input_type_call');
-let jobInput = document.querySelector('.popup__input_type_hobbies');
-let listContainer = document.querySelector('.elements__grid');
-let template = document.querySelector('.template')
-let deleteBtn = document.querySelector('.elements__dlt-btn');
-
-function render() {
-  const html = initialCards.map(getElement);
-  listContainer.append(...html);
-}
-
-function getElement(item) {
-  const getElementTemplate = template.content.cloneNode(true);
-  const templateName = getElementTemplate.querySelector('.elements__name');
-  const templateImage = getElementTemplate.querySelector('.elements__photo');
-  const deleteBtn = getElementTemplate.querySelector('.elements__dlt-btn');
-  const likeBtn = getElementTemplate.querySelector('.elements__like-btn')
-
-  templateImage.src = item.link;
-  templateName.textContent = item.name;
-
-  deleteBtn.addEventListener('click', removeElement);
-  likeBtn.addEventListener('click', likeElement);
-
-  return getElementTemplate;
-}
-
-function likeElement(){
-  this.classList.toggle('button_active');
-}
-
-
-function removeElement(evt){
-  const dltElement = evt.target.closest('.elements__item');
-  dltElement.remove();
-}
-
-function openModalWindow() {
-  modalWindow.classList.add('popup_is-active');
-  nameInput.value = bioName.textContent;
-  jobInput.value = bioDescription.textContent;
-}
-
-function closeModalWindow() {
-  modalWindow.classList.remove('popup_is-active');
-}
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  bioName.textContent = nameInput.value;
-  bioDescription.textContent = jobInput.value;
-  closeModalWindow();
-}
-
-editProfile.addEventListener('click', openModalWindow);
-modalCloseBtn.addEventListener('click', closeModalWindow);
-formElement.addEventListener('submit', formSubmitHandler);
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -87,6 +23,101 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-]; 
+];
+let editProfile = document.querySelector('.bio__edit-btn');
+let addCard = document.querySelector('.profile__add-btn');
+let popups = document.querySelectorAll('.popup');
+let popupCloseBtn = document.querySelectorAll('.popup__close-btn');
+let formElement = document.querySelector('.popup__form');
+let formImgCard = document.querySelector('.popup__img-card')
+let bioName = document.querySelector('.bio__name');
+let bioDescription = document.querySelector('.bio__description');
+let nameInput = document.querySelector('.popup__input_type_call');
+let jobInput = document.querySelector('.popup__input_type_hobbies');
+let placeInput = document.querySelector('.popup__input_type_place');
+let linkInput = document.querySelector('.popup__input_type_link');
+let listContainer = document.querySelector('.elements__grid');
+let template = document.querySelector('.template')
+let deleteBtn = document.querySelectorAll('.elements__dlt-btn');
+
+
+//Открытие попапа
+function togglePopup(index){
+  popups[index].classList.toggle("popup_is-active");
+  
+   if (popups[0]){
+    nameInput.value = bioName.textContent;
+    jobInput.value = bioDescription.textContent;
+  }
+}
+
+//Закрытие попапа
+function closePopups(index) {
+  popups[index].classList.remove('popup_is-active');
+}
+
+
+//Добавление карточки через template
+function render() {
+  const html = initialCards.map(getElement);
+  listContainer.append(...html);
+}
+
+function getElement(item) {
+  const getElementTemplate = template.content.cloneNode(true);
+  const templateName = getElementTemplate.querySelector('.elements__name');
+  const templateImage = getElementTemplate.querySelector('.elements__photo');
+  const deleteBtn = getElementTemplate.querySelector('.elements__dlt-btn');
+  const likeBtn = getElementTemplate.querySelector('.elements__like-btn')
+
+  templateImage.src = item.link;
+  templateName.textContent = item.name;
+
+  //Лайк карточки
+  function likeElement(){
+    this.classList.toggle('button_active');
+  }
+
+  //Удаление карточки
+  function removeElement(evt){
+    const dltElement = evt.target.closest('.elements__item');
+    dltElement.remove();
+  }
+
+  deleteBtn.addEventListener('click', removeElement);
+  likeBtn.addEventListener('click', likeElement);
+
+  return getElementTemplate;
+}
+
+
+//Форма отправки изменений в имени и описании профиля
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  bioName.textContent = nameInput.value;
+  bioDescription.textContent = jobInput.value;
+  closePopups(0);
+  }
+
+//Форма добавления фотокарточки
+function hundleAddCard(evt) {
+    evt.preventDefault();
+    let createCard = getElement({
+      name: placeInput.value,
+      link: linkInput.value
+    });
+    listContainer.prepend(createCard);
+    closePopups(1);
+    placeInput.value ='';
+    linkInput.value ='';
+  }
+
+//Кнопки
+editProfile.addEventListener('click', ()=> togglePopup(0));
+addCard.addEventListener('click', ()=> togglePopup(1));
+popupCloseBtn[0].addEventListener('click', ()=> closePopups(0));
+popupCloseBtn[1].addEventListener('click', ()=> closePopups(1));
+formElement.addEventListener('submit', formSubmitHandler);
+formImgCard.addEventListener('submit', hundleAddCard);
 
 render();
